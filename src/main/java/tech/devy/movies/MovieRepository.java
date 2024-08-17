e258492d255664ee0572f3a35858575b0bad03ff
DevanshUpadhyay26 <Devanshupadhyay26@gmail.com>
2024-08-17 19:16:50 +0530

    backend processed

---
+++ src/main/java/tech/devy/movies/MovieRepository.java
@@ -0,0 +1,15 @@
+package tech.devy.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+Optional<Movie> findMovieByImdbId(String imdbId);
+
+
+}
