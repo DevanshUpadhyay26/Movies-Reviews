d0aafa4e63a1b770cd42c8fc92a475f1b730191f
DevanshUpadhyay26 <Devanshupadhyay26@gmail.com>
2024-08-17 22:57:59 +0530

    -Added React and routes -Added Home Components

--- src/main/java/tech/devy/movies/ReviewController.java
+++ src/main/java/tech/devy/movies/ReviewController.java
@@ -5,13 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.EscapedErrors;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-
+@CrossOrigin("http://localhost:3000/")
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
