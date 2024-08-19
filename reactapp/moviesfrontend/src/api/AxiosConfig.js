6247d45b185f5b59ad0a7a881725136c63462be5
DevanshUpadhyay26 <Devanshupadhyay26@gmail.com>
2024-08-19 20:00:10 +0530

    Added: - Components - Axios config

--- reactapp/moviesfrontend/src/api/AxiosConfig.js
+++ reactapp/moviesfrontend/src/api/AxiosConfig.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create({
 
     // For not letting cors blocking 
-    baseURL:"https://f99f-2405-201-1d-60a4-6d18-15e0-c101-fb1f.ngrok-free.app/",
+    baseURL:"https://aac5-2405-201-1d-60a4-9d2b-12d8-2896-cabd.ngrok-free.app/",
     headers:{"ngrok-skip-browser-warning":"true"}       
 })
